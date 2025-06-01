e43e5a363e9524840898ffe25f203bf36907199c
zefparis <ben.barere@gmail.com>
2025-06-01 14:36:53 +0200

    Add React frontend structure and deployment configuration

---
+++ frontend-web/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Taxi Express</h1>
+        <p>
+          Bienvenue sur l'application Taxi Express
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
